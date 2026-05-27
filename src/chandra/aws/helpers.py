64265865ec266c8176727e9ca5d701a1f4148002
Maheswarareddy18 <164125445+Maheswarareddy18@users.noreply.github.com>
2026-05-27 14:45:27 +0530

    Update helpers.py

--- src/chandra/aws/helpers.py
+++ src/chandra/aws/helpers.py
@@ -3,7 +3,13 @@ from chandra.aws.client_factory import AwsClientFactory
 
 def get_factory_for_state(state):
 
-    factory = AwsClientFactory()
+    factory = get_factory_for_state(state)
+
+s3 = factory.client("s3")
+cloudwatch = factory.client("cloudwatch")
+iam = factory.client("iam")
+sns = factory.client("sns")
+sts = factory.client("sts")
 
     if state.assume_role_arn:
         return factory.assume_role(
