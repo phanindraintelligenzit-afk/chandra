6474e3360e30e96a74807c28111881d2df4ff58c
nagendrapanda45-cmyk <nagendrapanda45@gmail.com>
2026-05-27 17:54:08 +0530

    AWS-09: Add CloudWatch CIO demo dashboard JSON and Terraform

---
+++ iac/runtime/main.tf
@@ -0,0 +1,19 @@
+﻿terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_cloudwatch_dashboard" "chandra" {
+  dashboard_name = "Chandra"
+
+  dashboard_body = file("iac/{path.module}/dashboards/chandra.json")
+}
