6d80cc60b8d42f31ac8e3a4a0e6779cc21228859
Maheswarareddy18 <164125445+Maheswarareddy18@users.noreply.github.com>
2026-05-28 12:35:07 +0530

    Update variables.tf

--- iac/synthetic_env/modules/sns/variables.tf
+++ iac/synthetic_env/modules/sns/variables.tf
@@ -1,1 +1,15 @@
-variable "email" {}
+variable "alert_email" {
+  type = string
+}
+
+variable "project_name" {
+  default = "chandra"
+}
+
+variable "slack_workspace_id" {
+  type = string
+}
+
+variable "slack_channel_id" {
+  type = string
+}
