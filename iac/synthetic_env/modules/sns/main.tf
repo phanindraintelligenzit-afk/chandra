bf4f2e14f159b5867e46cdefda00563b00c34f85
Maheswarareddy18 <164125445+Maheswarareddy18@users.noreply.github.com>
2026-05-28 12:37:11 +0530

    Update main.tf

--- iac/synthetic_env/modules/sns/main.tf
+++ iac/synthetic_env/modules/sns/main.tf
@@ -7,3 +7,39 @@ resource "aws_sns_topic_subscription" "email" {
   protocol  = "email"
   endpoint  = var.email
 }
+####################################
+# SNS Topics
+####################################
+
+resource "aws_sns_topic" "critical" {
+  name = "${var.project_name}-escalation-critical"
+}
+
+resource "aws_sns_topic" "high" {
+  name = "${var.project_name}-escalation-high"
+}
+
+resource "aws_sns_topic" "warning" {
+  name = "${var.project_name}-escalation-warning"
+}
+####################################
+# Email Subscription
+####################################
+
+resource "aws_sns_topic_subscription" "critical_email" {
+  topic_arn = aws_sns_topic.critical.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
+}
+
+resource "aws_sns_topic_subscription" "high_email" {
+  topic_arn = aws_sns_topic.high.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
+}
+
+resource "aws_sns_topic_subscription" "warning_email" {
+  topic_arn = aws_sns_topic.warning.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
+}
